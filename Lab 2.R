fc921ab9c78c27b6bd91d3f5f1d7069abfc426ee
dondemahy <donald.demahy1@gmail.com>
2019-09-08 13:05:06 -0500

    Lab 2 exercises pre- fish data

---
+++ Lab 2.R
@@ -0,0 +1,39 @@
+x <- 7.5
+"word" -> y
+z = 10L
+w = factor(c("north","south","east","west"))
+z%%x
+#This is the modulus operator. Basically it gives the remainder of the division of z by x
+z%/%x
+#This operator gives the quotient of z/x to the nearest integer. It is, in a sense, the complement of the %% operator.
+x^z
+#This operator gives the value of x to the power of z to the nearest integer
+v = (1:10)
+{u=1;t=2;s=3}
+u*t+s
+u*(t+s)
+#Although both of these series of operations equal 5 in this context, they arrive at the value in different ways. 
+#R follows the typical order of operations, so in the first equation 'u' and 't' are getting multiplied first then that value is added to 's'. 
+#In the second case, due to the parentheses, 't' and 's' are getting added first and then multiplied by 'u'.
+
+#The values Inf, -Inf, NaN,and NA stand for infinity, negative infinity, not a number, and not available respectively.
+#Infinity and negative infinity are fairly self explanatory. Inf can be given as a response for several inputs. For example,
+3/0
+#Negative infinity might be given if the input was something like
+-5/0
+#NaN is given when the value asked for is not a real number. For example, if you input
+0/0
+#NA is used as a placeholder for values in a series you do not currently know.For example,
+c(5,9,12,12,NA)
+'%soi%' <- function(a,b){3*a+b^a}
+animal.choice <- function(x){
+  if (x == "a")
+    "giraffe"
+  else if (x == "b")
+    "kangaroo"
+  else if (x == "c")
+    "otter"
+  else
+    "birb"
+}
+
